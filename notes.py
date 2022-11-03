9190322019bd3d1259e4d998636159042a163ad4
Bruno Garcia <bgarcia@ascentio.com.ar>
2022-11-03 20:25:03 -0300

    main window is showing

--- notes.py
+++ notes.py
@@ -3,13 +3,12 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5.QtWidgets import QApplication
-from view.window import WindowController
+from view.window import NewWindowController
 import sys
 
 
 if __name__ == "__main__":
     """Ejecución de aplicación con interfaz gráfica."""
     app = QApplication(sys.argv)
-    ui = WindowController()
-    ui.main_window.show()
+    ui = NewWindowController(app)
     sys.exit(app.exec_())
