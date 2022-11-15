1fdb8fb171ae4b7062c7fc4ba750a31f8d06942a
Bruno Garcia <bgarcia@ascentio.com.ar>
2022-11-15 16:41:45 -0300

    login y signup forms se cargan al inicio

--- notes.py
+++ notes.py
@@ -2,15 +2,21 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QApplication
-from data.db_access import DbManager
-from view.main_window import WindowController
 import sys
 
+from PyQt5.QtWidgets import QApplication
+
+from data.db_access import DbManager
+from view.main_window import WellcomeDialog, WindowController
 
 if __name__ == "__main__":
     """Ejecución de aplicación con interfaz gráfica."""
     app = QApplication(sys.argv)
     dbm = DbManager()
-    ui = WindowController(dbm)
+    login = WellcomeDialog()
+
+    if login.exec_():
+        main = WindowController(dbm=dbm)
+        main.show()
+
     sys.exit(app.exec_())
--- view/main_window.py
+++ view/main_window.py
@@ -2,54 +2,98 @@
 # -*- coding: utf-8 -*-
 
 
-from PyQt5 import uic
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, uic
 
-
-from data.data_models import Note
-from data.data_models import User
-
+from data.data_models import Note, User
+from data.db_access import DbManager
 from logger.logger import log_try_exc_deco
 
 
-class WindowController:
+class WellcomeDialog(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("view/resource/wellcome.ui", self)
+
+    def accept(self):
+        # currentIndex = 1 -> signup
+        if self.tab.currentIndex():
+            usr = self.new_user_name.text()
+            paswd = self.new_user_pass_1.text()
+            conf = self.new_user_pass_2.text()
+
+            if usr == "" or paswd == "" or conf == "":
+                self.sig_error.setText(
+                    "Debe ingresar toda la informacion requerida"
+                )
+            elif " " in paswd:
+                self.sig_error.setText("No se admiten espacios en la clave")
+            elif paswd != conf:
+                self.sig_error.setText("Las claves no coinciden!")
+            elif not usr.isalnum():
+                self.sig_error.setText(
+                    "Usuario invalido. "
+                    "Solo se admiten caracteres alfanuméricos"
+                )
+            else:
+                print("Estamos creando una cuenta!!!")
+                self.done(1)
+
+        # currentIndex = 0 -> login
+        else:
+            usr = self.login_user_name.text()
+            paswd = self.login_user_pass.text()
+            if usr == "" or paswd == "":
+                self.log_error.setText(
+                    "Debe ingresar toda la informacion requerida"
+                )
+            else:
+                print("Estamos ingresando al sitio...")
+                self.done(1)
+
+    def reject(self):
+        print("Reject")
+        self.done(0)
+
+
+class WindowController(QtWidgets.QMainWindow):
     """Se definen los metodos de control de la ventana."""
 
-    def __init__(self, dbm):
+    def __init__(self, dbm: DbManager):
         """Controlador de la ventana principal de la aplicacion."""
 
+        super().__init__()
         self.dbm = dbm
 
-        self.layout = uic.loadUi("view/resource/main.ui")
+        uic.loadUi("view/resource/main.ui", self)
         self.new_usr_dialog = uic.loadUi("view/resource/new_user.ui")
 
         self.update_user_list()
         self.update_note_list()
         self.note = None
 
-        self.layout.user_list.setCurrentIndex(0)
+        self.user_list.setCurrentIndex(0)
         self.user = User(
-            user_name=self.layout.user_list.currentData(0),
-            user_id=self.layout.user_list.currentData(1)
+            user_name=self.user_list.currentData(0),
+            user_id=self.user_list.currentData(1)
         )
 
-        self.layout.btn_save.clicked.connect(self.save_note)
-        self.layout.btn_new.clicked.connect(self.new_note)
-        self.layout.btn_new_user.clicked.connect(self.new_user)
-        self.layout.btn_delete.clicked.connect(self.delete_selected_note)
-        self.layout.notes_list.itemClicked.connect(self.load_selected_note)
-        self.layout.user_list.currentIndexChanged.connect(self.user_changed)
+        self.btn_save.clicked.connect(self.save_note)
+        self.btn_new.clicked.connect(self.new_note)
+        self.btn_new_user.clicked.connect(self.new_user)
+        self.btn_delete.clicked.connect(self.delete_selected_note)
+        self.notes_list.itemClicked.connect(self.load_selected_note)
+        self.user_list.currentIndexChanged.connect(self.user_changed)
         self.new_usr_dialog.buttonBox.accepted.connect(self.new_user_accept)
         self.new_usr_dialog.buttonBox.rejected.connect(self.new_user_cancel)
 
-        self.layout.show()
+        self.show()
 
-    @log_try_exc_deco("save a note to database")
+    @ log_try_exc_deco("save a note to database")
     def save_note(self, *args):
         """Guarda Nota nueva o la actualiza si ya existe en la DB."""
         # Get Values from window
-        body = self.layout.notes_text.toPlainText()
-        title = self.layout.note_title.text()
+        body = self.notes_text.toPlainText()
+        title = self.note_title.text()
 
         if self.note is None:
             print(f'Saving new note with title: {title}')
@@ -82,8 +126,8 @@ class WindowController:
         """
         print('New note!!')
         self.note = None
-        self.layout.notes_text.setPlainText('')
-        self.layout.note_title.setText('')
+        self.notes_text.setPlainText('')
+        self.note_title.setText('')
 
     @log_try_exc_deco("open new user dialog")
     def new_user(self, *args):
@@ -93,6 +137,9 @@ class WindowController:
         la ventana de diálogo para ingresar el nombre.
         """
         print('Creating new user')
+        self.new_usr_dialog.new_user_name.setText("")
+        self.new_usr_dialog.new_user_pass_1.setText("")
+        self.new_usr_dialog.new_user_pass_2.setText("")
         self.new_usr_dialog.show()
 
     @log_try_exc_deco("save new user")
@@ -103,14 +150,55 @@ class WindowController:
         aceptar, se guarda el nombre de usuario en la DB.
         """
         print('Getting info about new user')
-        user_name = self.new_usr_dialog.line_new_user.text()
-        if user_name != '':
-            user = User(user_name=user_name)
+        user_name = self.new_usr_dialog.new_user_name.text()
+        passwd_1 = self.new_usr_dialog.new_user_pass_1.text()
+        passwd_2 = self.new_usr_dialog.new_user_pass_2.text()
+        names = [user.user_name for user in self.users]
+
+        if passwd_1 != passwd_2:
+            msg = QtWidgets.QMessageBox()
+            msg.setIcon(QtWidgets.QMessageBox.Critical)
+            msg.setText("Las claves no coinciden!")
+            msg.setInformativeText(
+                'Debe sescribir la misma clave en ambos campos.'
+            )
+            msg.setWindowTitle("ERROR")
+            msg.exec_()
+            self.new_usr_dialog.show()
+
+        elif not user_name.isalnum():
+            msg = QtWidgets.QMessageBox()
+            msg.setIcon(QtWidgets.QMessageBox.Critical)
+            msg.setText("Nombre de usuario no valido")
+            msg.setInformativeText(
+                'El usuario solo puede contener caratéres alfanumericos'
+            )
+            msg.setWindowTitle("ERROR")
+            msg.exec_()
+            self.new_usr_dialog.show()
+
+        elif user_name in names:
+            msg = QtWidgets.QMessageBox()
+            msg.setIcon(QtWidgets.QMessageBox.Critical)
+            msg.setText("El usuario ingresado ya existe")
+            msg.setInformativeText(
+                'Debe ingresar un usuario que no exista '
+                'previamente o hacer login si este ya es su usuario.'
+            )
+            msg.setWindowTitle("ERROR")
+            msg.exec_()
+            self.new_usr_dialog.show()
+
+        else:
+            user = User(
+                user_name=user_name,
+                password=passwd_1
+            )
             self.dbm.create_user(user)
             print(f'User {user.user_name} created')
 
-        self.update_user_list()
-        self.new_usr_dialog.close()
+            self.update_user_list()
+            self.new_usr_dialog.close()
 
     @log_try_exc_deco("cancel user creation")
     def new_user_cancel(self, *args):
@@ -125,49 +213,50 @@ class WindowController:
     def update_note_list(self, *args):
         """Actualización de listado de notas en la DB."""
         notes = self.dbm.get_list_of_notes()
-        self.layout.notes_list.clear()
+        self.notes_list.clear()
         for note in notes:
             itm = QtWidgets.QListWidgetItem(note.title)
             itm.setData(1, note.noteid)
-            self.layout.notes_list.addItem(itm)
+            self.notes_list.addItem(itm)
             print(f'Updating List with Note: {note.title}')
 
     @log_try_exc_deco("retrieve users from database")
     def update_user_list(self, *args):
         """Actualización de listado de usuarios en la DB."""
-        users = self.dbm.get_list_of_users()
-        self.layout.user_list.clear()
-        for index, user in enumerate(users):
-            self.layout.user_list.addItem(user.user_name)
-            self.layout.user_list.setItemData(index, user.user_id, role=1)
+        self.users = self.dbm.get_list_of_users()
+        self.user_list.clear()
+        for index, user in enumerate(self.users):
+            self.user_list.addItem(user.user_name)
+            self.user_list.setItemData(
+                index, user.user_id, role=1)
 
         print('Updating User List')
 
     @log_try_exc_deco("change selected user")
     def user_changed(self, *args):
         """Actualiza el usuario seleccionado dentro del controlador."""
-        user_name = self.layout.user_list.currentData(0)
-        user_id = self.layout.user_list.currentData(1)
+        user_name = self.user_list.currentData(0)
+        user_id = self.user_list.currentData(1)
         self.user = User(user_id, user_name)
         print(f'Selected User: {user_name} with id: {user_id}')
 
     @log_try_exc_deco("load note")
     def load_selected_note(self, *args):
         """Carga el contenido y los datos de la nota seleccionada en la UI."""
-        selected = self.layout.notes_list.selectedItems()[0]
+        selected = self.notes_list.selectedItems()[0]
         note = self.dbm.get_note_from_id(
             selected.data(1)
         )
         self.note = note
-        self.layout.notes_text.setPlainText(note.body)
-        self.layout.note_title.setText(note.title)
+        self.notes_text.setPlainText(note.body)
+        self.note_title.setText(note.title)
         print(f'Loading Note: {note.title}')
 
     @log_try_exc_deco("delete selected note")
     def delete_selected_note(self, *args):
         """Elimina de la DB la nota seleccionada."""
         try:
-            selected = self.layout.notes_list.selectedItems()[0]
+            selected = self.notes_list.selectedItems()[0]
             self.dbm.delete_note(int(selected.data(1)))
             print(f'Deleting Note: {selected.text()}')
             self.update_note_list()
